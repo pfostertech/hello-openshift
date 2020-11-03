b4b560ad8f049647429db52854f99b41dffa8bc5
Philip <philip.foster@technologent.com>
2020-11-02 18:25:26 -0600

    Remove unused imports

--- src/test/java/com/example/demo/TestHealthCheck.java
+++ src/test/java/com/example/demo/TestHealthCheck.java
@@ -5,12 +5,9 @@ import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER
 import com.example.demo.health.AppHealthIndicator;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Status;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
